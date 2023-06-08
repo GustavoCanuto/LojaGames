a75d77069d6d783590b55995c3e925b9ecec325b
Gustavo Henrique Canuto Cruz <131610213+GustavoCanuto@users.noreply.github.com>
2023-06-08 20:14:04 -0300

    initial commit

---
+++ ajax.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function($) {
+    // Função para carregar o conteúdo do carrosel.html
+    function carregarConteudo() {
+        $.ajax({
+            url: 'carrosel.html',
+        })
+        .done(function(html) {
+            $("#conteudo").empty().append(html);
+        })
+        .fail(function() {
+            console.log("erro");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+    }
+
+    // Carregar conteúdo ao carregar a página
+    carregarConteudo();
+
+    // Manipulador de evento para o clique em link
+    $("a").click(function(event){
+        link=$(this).attr("href");
+        $.ajax({
+            url: link,
+
+        })
+        .done(function(html){
+            $("#conteudo").empty().append(html);
+
+        }
+        )
+        .fail(function(){
+            console.log("erro");
+        })
+        .always(function(){
+            console.log("complete");
+
+        });
+        return false;
+
+    });
+
+});
